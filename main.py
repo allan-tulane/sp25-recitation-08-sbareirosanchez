4f7af6de678d0a9299de220e85e3b975ce5724f1
sbareirosanchez <sbareirosanchez@tulane.edu>
2025-05-02 22:45:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,17 @@ def shortest_shortest_path(graph, source):
       a dict where each key is a vertex and the value is a tuple of
       (shortest path weight, shortest path number of edges). See test case for example.
     """
-    ### TODO
-    pass
+    visited = {}
+    heap = [(0,0, source)]
+    while heap: 
+        weight, edges, node = heappop(heap)
+        if node in visited: 
+            continue
+        visited[node] = (weight, edges)
+        for neighbor, w in graph.get(node, set()):
+            if neighbor not in visited:
+                heappush(heap, (weight + w, edges +1, neighbor))
+    return visited
     
 
     
@@ -24,8 +33,15 @@ def bfs_path(graph, source):
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
-    ###TODO
-    pass
+    parents = {source: None}
+    queue = deque([source])
+    while queue:
+        current = queue.popleft()
+        for neighbor in graph.get(current, set()):
+            if neighbor not in parents:
+                parents[neighbor] = current
+                queue.append(neighbor)
+    return parents
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -43,6 +59,12 @@ def get_path(parents, destination):
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
+    path = []
+    while destination in parents and parents[destination] is not None: 
+        destination = parents[destination]
+        path.insert(0, destination)
+    return ''.join(path)
+    
+    
+    
 
